a9298a6cfe5eda823bd20e7734a68280642be8c4
Matt Hale <matthew.hale@protonmail.com>
2019-12-17 10:53:26 -0500

    Add original character shell script
    This is mostly for reference; functionality and data will be replicated from
    this.

---
+++ carrion_crown_fighter.sh
@@ -0,0 +1,363 @@
+### CHARACTER VARIABLES ###
+
+# Informational
+name="Qofin Parora"
+race="half-elf"
+class="fighter"
+
+# A half-elf pack mule
+# Works closely with traveling merchants of the area
+# Skilled at smuggling, concealing daggers, etc.
+
+# 5'11"
+# 160 lbs.
+
+# Base ability scores
+abilities="str=17
+dex=16
+con=16
+int=13
+wis=10
+cha=11"
+
+lvl=1
+hitDie=10
+maxHP=13
+baseAttackBonus=1
+
+# Traits
+
+traits="On the payroll: +150 starting gold
+Ordinary: +4 to stealth checks when attempting to in a crowd"
+
+# Feats
+feats="Skill Focus: +3 to checks with chosen skill (sleight of hand)
+Deft Hands: +2 to Disable Device and Sleight of Hand checks; bonus increases to +4 with 10 or more ranks in these skills"
+
+# Gold
+gold=35
+silver=4
+copper=8
+
+# Equipment
+equipment="Tent, hanging: weight=15(lbs) count=1 pack=0
+Outfit, traveler's: weight=5(lbs) count=1 pack=1
+Backpack, masterwork: weight=4(lbs) capacity=2(cubic feet) count=1 pack=1 special=(Treat strength as 1 higher for carrying capacity)
+Bedroll: weight=5(lbs) count=1 pack=0
+Blanket: weight=3(lbs) count=1 pack=0
+Fishing tackle: weight=5(lbs) count=1 pack=0 special=(+1 circumstance bonus to survival checks to gather food around bodies of water that contain fish)
+Crowbar: weight=5(lbs) count=1 pack=1 special=(+2 circumstance bonus to strength checks to force open a door or chest)
+Pouch, belt: weight=1(lbs) capacity=1/5(cubic feet) count=1 pack=1
+Scarf, pocketed: weight=1(lbs) count=1 pack=1 special=(+4 bonus on Sleight of Hand checks made to hide objects on your body)
+Flint & steel: count=1 pack=1
+Pot, cooking, iron: weight=2(lbs) count=1 pack=0 special=(holds 1 gallon)
+Kit, mess: weight=1(lbs) count=1 pack=0
+Tools, artisan's: weight=5(lbs) count=1 pack=0
+Rope, hemp, 50 ft: count=1 weight=10(lbs) pack=1
+Twine, 100 ft: count=1 weight=1(lbs) pack=1
+Soap: weight=1(lbs) count=1 pack=0
+Torch: count=5 weight=1(lbs) pack=1
+Compass: count=1 weight=0(lbs) pack=1 special=(+1 to survival checks made to avoid becoming lost, and +1 to KnowledgeDungeoneering checks made underground for navigation)
+Rations, trail: count=5 weight=1(lbs) pack=1
+Waterskin: capacity=1(gallon) count=1 pack=1
+Reinforced tunic: weight=5(lbs) count=1 pack=1
+Sword, short: weight=2(lbs) count=1 pack=1
+Shield, light wooden: weight=5(lbs) count=1 pack=1
+Longspear: weight=9(lbs) count=1 pack=1
+Dagger: weight=2(lbs) count=2 pack=1
+Crossbow, light: weight=4(lbs) count=2 pack=1
+Bolts, crossbow: weight=1(lbs) count=30 pack=1"
+
+# Weapons
+weapons="Sword, short: damage=1d6 critRoll=19 critMulti=2 damageType=piercing
+Longspear: damage=1d8 critRoll=20 critMulti=3 damageType=piercing special=brace,reach 2h
+Dagger: damage=1d4 critRoll=19 critMulti=3 damageType=piercing,slashing
+Crossbow, light: damage=1d8 critRoll=19 critMulti=2 damageType=piercing ammo=20 ranged"
+
+armor="Reinforced tunic: ac=1 maxDex=5 penalty=0 special=(+2 to AC against crit confirmation rolls against you)
+Shield, light wooden: ac=1 penalty=-1"
+
+# Skills
+skills="Acrobatics: rank=0 isClass=0 mod=dex bonus=0
+Appraise: rank=0 isClass=0 mod=int bonus=0
+Bluff: rank=0 isClass=1 mod=cha bonus=0
+Climb: rank=0 isClass=1 mod=str bonus=0
+Craft: rank=1 isClass=1 mod=int bonus=0 special=weapons
+Diplomacy: rank=0 isClass=0 mod=cha bonus=0
+DisableDevice: rank=0 isClass=0 mod=dex bonus=2
+Disguise: rank=0 isClass=1 mod=cha bonus=0
+EscapeArtist: rank=0 isClass=0 mod=dex bonus=0
+Fly: rank=0 isClass=0 mod=dex bonus=0
+HandleAnimal: rank=0 isClass=1 mod=cha bonus=0
+Heal: rank=0 isClass=0 mod=wis bonus=0
+Intimidate: rank=1 isClass=1 mod=cha bonus=0
+KnowledgeArcana: rank=0 isClass=0 mod=int bonus=0
+KnowledgeDungeoneering: rank=0 isClass=1 mod=int bonus=0
+KnowledgeEngineering: rank=0 isClass=1 mod=int bonus=0
+KnowledgeGeography: rank=0 isClass=0 mod=int bonus=0
+KnowledgeHistory: rank=0 isClass=0 mod=int bonus=0
+KnowledgeLocal: rank=0 isClass=0 mod=int bonus=0
+KnowledgeNature: rank=0 isClass=0 mod=int bonus=0
+KnowledgeNobility: rank=0 isClass=0 mod=int bonus=0
+KnowledgePlanes: rank=0 isClass=0 mod=int bonus=0
+KnowledgeReligion: rank=0 isClass=0 mod=int bonus=0
+Linguistics: rank=0 isClass=0 mod=int bonus=0
+Perception: rank=0 isClass=0 mod=wis bonus=0
+Perform: rank=0 isClass=0 mod=cha bonus=0
+Profession: rank=1 isClass=1 mod=wis bonus=0 special=smuggler
+Ride: rank=0 isClass=1 mod=dex bonus=0
+SenseMotive: rank=0 isClass=0 mod=wis bonus=0
+SleightOfHand: rank=1 isClass=1 mod=dex bonus=5
+Spellcraft: rank=0 isClass=0 mod=int bonus=0
+Stealth: rank=1 isClass=1 mod=dex bonus=0
+Survival: rank=1 isClass=1 mod=wis bonus=0
+Swim: rank=0 isClass=1 mod=str bonus=0
+UseMagicDevice: rank=0 isClass=0 mod=cha bonus=0"
+
+###########################
+
+
+### FUNCTIONS ###
+IFS="
+"
+
+# Wrapper for getting a random integer from urandom
+getRandom() {
+    od -vAn -N2 -tu2 < /dev/urandom | grep  -o "[0-9]*"
+}
+
+# Roll an X sided die; defaults to a d20
+rollDice() {
+    arg=$(echo "$1" | grep -o "[0-9]*")
+    if [ -z "$arg" ]; then
+        size=20
+    else
+        size="$arg"
+    fi
+
+    random=$(getRandom)
+    result=$(( (random % size) + 1 ))
+    echo "$result"
+}
+
+# Takes a given integer and returns the ability modifier
+getAbilityMod() {
+    case "$1" in
+        1)
+            echo "-5"
+            ;;
+        [2-3])
+            echo "-4"
+            ;;
+        [4-5])
+            echo "-3"
+            ;;
+        [6-7])
+            echo "-2"
+            ;;
+        [8-9])
+            echo "-1"
+            ;;
+        1[0-1])
+            echo "0"
+            ;;
+        1[2-3])
+            echo "1"
+            ;;
+        1[4-5])
+            echo "2"
+            ;;
+        1[6-7])
+            echo "3"
+            ;;
+        1[8-9])
+            echo "4"
+            ;;
+        2[0-1])
+            echo "5"
+            ;;
+    esac
+}
+
+# Display skills
+getSkills() {
+    out=""
+    for item in $skills; do
+        out="${out}${item}\n"
+    done
+    echo "$out"
+}
+
+# Display feats
+getFeats() {
+    out=""
+    for item in $feats; do
+        out="${out}${item}\n"
+    done
+    echo "$out"
+}
+
+# Display formatted list of weapons
+getWeapons() {
+    out=""
+    for item in $weapons; do
+        name=$(echo "$item" | grep -o "[a-zA-Z, ]*\:")
+        damage=$(echo "$item" | grep -o "damage=[0-9]d[0-9]*" | sed "s/damage=//")
+        crit=$(echo "$item" | grep -o "critRoll=[0-9]*" | sed "s/critRoll=//")
+        critMulti=$(echo "$item" | grep -o "critMulti=[0-9]*" | sed "s/critMulti=//")
+        damageType=$(echo "$item" | grep -o "damageType=[a-zA-Z,]*" | sed "s/damageType=//")
+        damageType=$(echo "$damageType" | sed "s/,/, /g")
+        if [ "$crit" -ne 20 ]; then
+            crit="${crit}-20 x${critMulti}"
+        else
+            crit="x${critMulti}"
+        fi
+        out="${out}${name} ${damage}, ${crit} (${damageType})\n"
+    done
+    echo "$out"
+}
+
+# Display formatted list of equipment
+getEquipment() {
+    out=""
+    packWeight=0
+    for item in $equipment; do
+        name=$(echo "$item" | grep -o "[0-9a-zA-Z,&' ]*\:" | sed "s/\://")
+        count=$(echo "$item" | grep -o "count=[0-9]*" | sed "s/count=//")
+        unitWeight=$(echo "$item" | grep -o "weight=[0-9]*" | sed "s/weight=//")
+        subtotalWeight=$(( unitWeight * count ))
+        pack=$(echo "$item" | grep -o "pack=[0-1]" | sed "s/pack=//")
+        if [ "$pack" -eq 1 ]; then
+            out="${out}${name} x${count}, ${unitWeight} lbs each (${subtotalWeight} lbs)\n"
+            packWeight=$(( packWeight + subtotalWeight ))
+        else
+            out="${out}${name} x${count}, ${unitWeight} lbs each (${subtotalWeight} lbs) - camp item\n"
+        fi
+    done
+    totalWeight=$(getTotalWeight)
+    out="${out}Total weight with camp set up: ${packWeight} lbs\nTotal equipment weight: ${totalWeight} lbs\n"
+    echo "$out"
+}
+
+# Total up all the weight of your character's equipment
+getTotalWeight() {
+    total=0
+    for item in $equipment; do
+        count=$(echo "$item" | grep -o "count=[0-9]*" | sed "s/count=//")
+        unitWeight=$(echo "$item" | grep -o "weight=[0-9]*" | sed "s/weight=//")
+        total=$(( count * unitWeight + total ))
+    done
+    echo "${total}"
+}
+
+#################
+
+### MAIN ###
+
+case "$1" in
+    roll)
+        case "$2" in
+            h|help)
+                out="Usage: $0 roll {attack|<skill>|<dice size>|help}\nDefaults to rolling a d20"
+                ;;
+            attack)
+                weaponCount=1
+                for item in $weapons; do
+                    env printf "${weaponCount}: $item\n"
+                    weaponCount=$(( weaponCount + 1 ))
+                done
+                choice=0
+                while [ "$choice" -lt 1 ] || [ "$choice" -gt "$weaponCount" ]; do
+                    env printf "Choose weapon: "
+                    read -r choice
+                    match=$(echo "$choice" | grep "[1-9]")
+                    if [ -z "$match" ]; then
+                        choice=0
+                    fi
+                done
+                weaponChoice=""
+                i=1
+                for item in $weapons; do
+                    if [ "$i" -eq "$choice" ]; then
+                        weaponChoice="$item"
+                    fi
+                    i=$(( i + 1 ))
+                done
+                isRanged=$(echo "$weaponChoice" | grep "ranged")
+                if [ -n "$isRanged" ]; then
+                    ability=$(echo $abilities | grep -o "dex=[0-9]*" | sed "s/dex=//")
+                    mod=$(getAbilityMod $ability)
+                else
+                    ability=$(echo $abilities | grep -o "str=[0-9]*" | sed "s/str=//")
+                    mod=$(getAbilityMod $ability)
+                fi
+                rollResult=$(rollDice 20)
+                critRoll=$(echo "$weaponChoice" | grep -o "critRoll=[0-9]*" | sed "s/critRoll=//")
+                if [ "$rollResult" -ge "$critRoll" ]; then
+                    out="crit (rolled ${rollResult})"
+                else
+                    out=$(( rollResult + baseAttackBonus + mod ))
+                fi
+                ;;
+            [a-zA-Z]*)
+                skill=$(echo "$skills" | grep "^${2}:")
+                if [ -z "$skill" ]; then
+                    out="No skill named ${2}"
+                else
+                    rollResult=$(rollDice 20)
+                    skillRanks=$(echo "$skill" | grep -o "rank=[0-9]*" | sed "s/rank=//")
+                    isClass=$(echo "$skill" | grep -o "isClass=1")
+                    if [ -n "$isClass" ] && [ "$skillRanks" -gt 0 ]; then
+                        skillRanks=$(( skillRanks + 3 ))
+                    fi
+                    bonus=$(echo "$skill" | grep -o "bonus=[0-9]")
+                    skillMod=$(echo "$skill" | grep -o "mod=[a-zA-Z]*" | sed "s/mod=//")
+                    ability=$(echo $abilities | grep -o "$skillMod=[0-9]*" | sed "s/$skillMod=//")
+                    modValue=$(getAbilityMod "$ability")
+                    out=$(( rollResult + skillRanks + bonus + modValue ))
+                fi
+                ;;
+            [1-9]|[1-9][0-9]|[1-9][0-9][0-9])
+                out=$(rollDice "$2")
+                ;;
+            *)
+                out=$(rollDice 20)
+                ;;
+        esac
+        env printf "${out}\n"
+        ;;
+    list)
+        case "$2" in
+            abilities)
+                out=""
+                for a in $abilities; do
+                    value=$(echo "$a" | grep -o "[0-9]*")
+                    mod=$(getAbilityMod $value)
+                    if [ "$mod" -gt 0 ]; then
+                        mod="+${mod}"
+                    fi
+                    out="${out}$a ($mod)\n"
+                done
+                ;;
+            skills)
+                out=$(getSkills)
+                ;;
+            equipment|items)
+                out=$(getEquipment)
+                ;;
+            weapons)
+                out=$(getWeapons)
+                ;;
+            feats)
+                out=$(getFeats)
+                ;;
+            *)
+                out="Usage: $0 list {abilities|skills|items|weapons|feats}\n"
+                ;;
+        esac
+        env printf "$out"
+        ;;
+    *)
+        echo "Usage: $0 {list|roll}"
+        ;;
+esac
