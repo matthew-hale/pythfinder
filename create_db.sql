c27a34c5d1e5333f908e00549779bb7b3b412686
Matt Hale <matthew.hale@protonmail.com>
2019-12-17 12:21:58 -0500

    Create multiple tables, an ability type, and initial skills data

--- create_db.sql
+++ create_db.sql
@@ -1,3 +1,12 @@
+CREATE TYPE ability AS ENUM (
+    'strength',
+    'dexterity',
+    'constitution',
+    'intelligence',
+    'wisdom',
+    'charisma'
+);
+
 CREATE TABLE characters (
     name text PRIMARY KEY, 
     race text NOT NULL,
@@ -7,7 +16,7 @@ CREATE TABLE characters (
     base_attack_bonus integer DEFAULT 1
 );
 
-CREATE TABLE abilities (
+CREATE TABLE character_abilities (
     character text PRIMARY KEY references characters(name),
     strength int CHECK (strength > 0 AND strength < 99) DEFAULT 10,
     dexterity int CHECK (dexterity > 0 AND dexterity < 99) DEFAULT 10,
@@ -15,6 +24,58 @@ CREATE TABLE abilities (
     intelligence int CHECK (intelligence > 0 AND intelligence < 99) DEFAULT 10,
     wisdom int CHECK (wisdom > 0 AND wisdom < 99) DEFAULT 10,
     charisma int CHECK (charisma > 0 AND charisma < 99) DEFAULT 10
+);
+
+CREATE TABLE skills (
+    name text PRIMARY KEY,
+    description text DEFAULT '',
+    use_untrained boolean NOT NULL,
+    modifier ability NOT NULL,
+    ac_penalty boolean NOT NULL
+);
+
+INSERT INTO skills VALUES ('Acrobatics', '', true, 'dexterity', true);
+INSERT INTO skills VALUES ('Appraise', '', true, 'intelligence', false);
+INSERT INTO skills VALUES ('Bluff', '', true, 'charisma', false);
+INSERT INTO skills VALUES ('Climb', '', true, 'strength', true);
+INSERT INTO skills VALUES ('Craft', '', true, 'intelligence', false);
+INSERT INTO skills VALUES ('Diplomacy', '', true, 'charisma', false);
+INSERT INTO skills VALUES ('Disable Device', '', false, 'dexterity', true);
+INSERT INTO skills VALUES ('Disguise', '', true, 'charisma', false);
+INSERT INTO skills VALUES ('Escape Artist', '', true, 'dexterity', true);
+INSERT INTO skills VALUES ('Fly', '', true, 'dexterity', true);
+INSERT INTO skills VALUES ('Handle Animal', '', false, 'charisma', false);
+INSERT INTO skills VALUES ('Heal', '', true, 'wisdom', false);
+INSERT INTO skills VALUES ('Intimidate', '', true, 'charisma', false);
+INSERT INTO skills VALUES ('Knowledge (arcana)', '', false, 'intelligence', false);
+INSERT INTO skills VALUES ('Knowledge (dungeoneering)', '', false, 'intelligence', false);
+INSERT INTO skills VALUES ('Knowledge (engineering)', '', false, 'intelligence', false);
+INSERT INTO skills VALUES ('Knowledge (geography)', '', false, 'intelligence', false);
+INSERT INTO skills VALUES ('Knowledge (history)', '', false, 'intelligence', false);
+INSERT INTO skills VALUES ('Knowledge (local)', '', false, 'intelligence', false);
+INSERT INTO skills VALUES ('Knowledge (nature)', '', false, 'intelligence', false);
+INSERT INTO skills VALUES ('Knowledge (nobility)', '', false, 'intelligence', false);
+INSERT INTO skills VALUES ('Knowledge (planes)', '', false, 'intelligence', false);
+INSERT INTO skills VALUES ('Knowledge (religion)', '', false, 'intelligence', false);
+INSERT INTO skills VALUES ('Linguistics', '', false, 'intelligence', false);
+INSERT INTO skills VALUES ('Perception', '', true, 'wisdom', false);
+INSERT INTO skills VALUES ('Perform', '', true, 'charisma', false);
+INSERT INTO skills VALUES ('Profession', '', false, 'wisdom', false);
+INSERT INTO skills VALUES ('Ride', '', true, 'dexterity', true);
+INSERT INTO skills VALUES ('Sense Motive', '', true, 'wisdom', false);
+INSERT INTO skills VALUES ('Sleight of Hand', '', false, 'dexterity', true);
+INSERT INTO skills VALUES ('Spellcraft', '', false, 'intelligence', false);
+INSERT INTO skills VALUES ('Stealth', '', true, 'dexterity', true);
+INSERT INTO skills VALUES ('Survival', '', true, 'wisdom', false);
+INSERT INTO skills VALUES ('Swim', '', true, 'strength', true);
+INSERT INTO skills VALUES ('Use Magic Device', '', false, 'charisma', false);
+
+CREATE TABLE character_skills (
+    character text references characters(name),
+    name text references skills(name),
+    ranks integer CHECK (ranks >= 0) DEFAULT 0,
+    is_class boolean DEFAULT false,
+    profession_or_craft_type text DEFAULT ''
 );
 
 CREATE TABLE items (
@@ -63,10 +124,3 @@ CREATE TABLE character_feats (
     count integer CHECK (count >= 1) DEFAULT 1
 );
 
-INSERT INTO feats VALUES (
-    'Test feat',
-    'This is a test feat.',
-    1,
-    'skill',
-    'acrobatics'
-);
