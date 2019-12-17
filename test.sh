e5d1d2e0c91db46d38440474b6a6af5af7734038
Matt Hale <matthew.hale@protonmail.com>
2019-12-17 15:48:34 -0500

    Test skill retrieval and bonus calculation

--- test.sh
+++ test.sh
@@ -5,4 +5,29 @@
 # Script for testing out the database
 
 skills=$(psql pathfinder --csv -c "SELECT name, ranks, is_class, profession_or_craft_type FROM character_skills WHERE character = 'Qofin Parora'" | tail -n +2)
-echo "$skills"
+
+out="Current skill bonuses:\n"
+
+IFS="
+"
+for skill in $skills; do
+    skillName=$(echo "$skill" | cut -d "," -f 1)
+    ranks=$(echo "$skill" | cut -d "," -f 2)
+    isClass=$(echo "$skill" | cut -d "," -f 3)
+    isCraftOrProfession=$(echo "$skill" | cut -d "," -f 4)
+    if [ "$isClass" = "t" ]; then
+        if [ "$ranks" -ge 1 ]; then
+            bonus=$(( ranks + 3 ))
+        else
+            bonus="$ranks"
+        fi
+    else
+        bonus="$ranks"
+    fi
+    if [ -n "$isCraftOrProfession" ]; then
+        skillName="${skillName} (${isCraftOrProfession})"
+    fi
+    out="${out}${skillName}: ${bonus}\n"
+done
+
+env printf "$out"
