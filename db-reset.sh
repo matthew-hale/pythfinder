2b74faad8df5e94ca00fa5a82590d44f68f687a9
Matt Hale <matthew.hale@protonmail.com>
2019-12-26 09:21:07 -0500

    Remove everything database related
    Following through on previous commit, I've decided to just get it over with
    and remove all the database stuff right now. I'll probably write up a spec for
    json-based character sheets, but that'll be it in terms of standardization.

--- db-reset.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# dbcleanup.sh
-#
-# Deletes and creates the pathfinder database for testing
-
-psql postgres -c "DROP DATABASE pathfinder"
-createdb pathfinder
-psql pathfinder -f ./create_db.sql
