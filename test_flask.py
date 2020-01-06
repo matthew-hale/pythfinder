db7d7ab0d8528c388d17e20ea4be364cad9b7063
Matt Hale <matthew.hale@protonmail.com>
2020-01-06 15:10:29 -0500

    Add API endpoints for simple character properties

--- test_flask.py
+++ test_flask.py
@@ -11,21 +11,8 @@ app.secret_key = "super_secret_development_key.jpg"
 def index():
     if request.method == "POST":
         session["character"] = request.form["character"]
-        characterData = json.loads(session["character"])
-        character = pf.Character(data = characterData)
-        characterShort = character.getCharacterShort()
-        response = make_response(render_template("index.html", character = characterShort))
-        return response
-    else:
-        if "character" in session:
-            characterData = json.loads(session["character"])
-            character = pf.Character(data = characterData)
-            characterShort = character.getCharacterShort()
-            response = make_response(render_template("index.html", character = characterShort))
-            return response
-        else:
-            character = ""
-            return render_template("index.html", character = character)
+    response = make_response(render_template("index.html"))
+    return response
 
 @app.route("/character")
 def get_character():
@@ -57,3 +44,124 @@ def get_homeland():
         return character.homeland
     else:
         return ""
+
+@app.route("/deity")
+def get_deity():
+    if "character" in session:
+        character = pf.Character(json.loads(session["character"]))
+        return character.deity
+    else:
+        return ""
+
+@app.route("/CMB")
+def get_CMB():
+    if "character" in session:
+        character = pf.Character(json.loads(session["character"]))
+        return character.CMB
+    else:
+        return ""
+
+@app.route("/CMD")
+def get_CMD():
+    if "character" in session:
+        character = pf.Character(json.loads(session["character"]))
+        return character.CMD
+    else:
+        return ""
+
+@app.route("/alignment")
+def get_alignment():
+    if "character" in session:
+        character = pf.Character(json.loads(session["character"]))
+        return character.alignment
+    else:
+        return ""
+
+@app.route("/description")
+def get_description():
+    if "character" in session:
+        character = pf.Character(json.loads(session["character"]))
+        return character.description
+    else:
+        return ""
+
+@app.route("/height")
+def get_height():
+    if "character" in session:
+        character = pf.Character(json.loads(session["character"]))
+        return character.height
+    else:
+        return ""
+
+@app.route("/weight")
+def get_weight():
+    if "character" in session:
+        character = pf.Character(json.loads(session["character"]))
+        return character.weight
+    else:
+        return ""
+
+@app.route("/size")
+def get_size():
+    if "character" in session:
+        character = pf.Character(json.loads(session["character"]))
+        return character.size
+    else:
+        return ""
+
+@app.route("/age")
+def get_age():
+    if "character" in session:
+        character = pf.Character(json.loads(session["character"]))
+        return character.age
+    else:
+        return ""
+
+@app.route("/initiativeMods")
+def get_initiativeMods():
+    if "character" in session:
+        character = pf.Character(json.loads(session["character"]))
+        return character.initiativeMods
+    else:
+        return ""
+
+@app.route("/hair")
+def get_hair():
+    if "character" in session:
+        character = pf.Character(json.loads(session["character"]))
+        return character.hair
+    else:
+        return ""
+
+@app.route("/eyes")
+def get_eyes():
+    if "character" in session:
+        character = pf.Character(json.loads(session["character"]))
+        return character.eyes
+    else:
+        return ""
+
+@app.route("/languages")
+def get_languages():
+    if "character" in session:
+        character = pf.Character(json.loads(session["character"]))
+        return character.languages
+    else:
+        return ""
+
+@app.route("/baseAttackBonus")
+def get_baseAttackBonus():
+    if "character" in session:
+        character = pf.Character(json.loads(session["character"]))
+        return character.baseAttackBonus
+    else:
+        return ""
+
+@app.route("/gold")
+def get_gold():
+    if "character" in session:
+        character = pf.Character(json.loads(session["character"]))
+        return character.gold
+    else:
+        return ""
+
