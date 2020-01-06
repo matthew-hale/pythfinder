6349f12ba5798045ddb306051162873859379e9a
Matt Hale <matthew.hale@protonmail.com>
2020-01-06 14:04:54 -0500

    Rework web app
    I've decided to implement plan b: use flask sessions and don't worry about
    storing data anywhere, which simplifies the process.
    
    I've created an extremely barebones API, with a corresponding barebones web
    page that uses javascript to make get requests to the API endpoints to fill in
    the paragraph elements on the page. Future implementations will:
    
    + Have a full range of API endpoints for the character data
    + Present the data via nicely formmated web form
    + Handle POST requests for user data input and blank character sheet creation

--- test_flask.py
+++ test_flask.py
@@ -1,24 +1,59 @@
 import pythfinder as pf
 import json
-from flask import Flask, request, render_template, make_response
+from flask import Flask, session, request, render_template, make_response
 
 app = Flask(__name__)
 
+# Testing purposes only (duh)
+app.secret_key = "super_secret_development_key.jpg"
+
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "GET":
-        return render_template("form.html")
-    else:
-        characterData = json.loads(request.form["character"])
+    if request.method == "POST":
+        session["character"] = request.form["character"]
+        characterData = json.loads(session["character"])
         character = pf.Character(data = characterData)
         characterShort = character.getCharacterShort()
         response = make_response(render_template("index.html", character = characterShort))
-        cookie_str = character.getJson()
-        response.set_cookie(key = "character", value = cookie_str)
-
         return response
+    else:
+        if "character" in session:
+            characterData = json.loads(session["character"])
+            character = pf.Character(data = characterData)
+            characterShort = character.getCharacterShort()
+            response = make_response(render_template("index.html", character = characterShort))
+            return response
+        else:
+            character = ""
+            return render_template("index.html", character = character)
 
-@app.route('/character/')
+@app.route("/character")
 def get_character():
-    response = make_response("Character cookie:\n{}".format(request.cookies.get('character')))
-    return response
+    if "character" in session:
+        return session["character"]
+    else:
+        return ""
+
+@app.route("/name")
+def get_name():
+    if "character" in session:
+        character = pf.Character(json.loads(session["character"]))
+        return character.name
+    else:
+        return ""
+
+@app.route("/race")
+def get_race():
+    if "character" in session:
+        character = pf.Character(json.loads(session["character"]))
+        return character.race
+    else:
+        return ""
+
+@app.route("/homeland")
+def get_homeland():
+    if "character" in session:
+        character = pf.Character(json.loads(session["character"]))
+        return character.homeland
+    else:
+        return ""
