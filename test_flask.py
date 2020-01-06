aadff21bd5f29df53215d5633975a5a3642e03c8
Matt Hale <matthew.hale@protonmail.com>
2020-01-06 11:50:53 -0500

    Improve demo functionality of flask app
    The intention here is to create the API using Flask, and then have a proper
    front-end layer on top that makes requests to the Flask API endpoints. I'm
    trying to work through the process of handling user data across requests; I
    want to use a simple cookie, but I'm getting issues with handling them. For
    now, the dev Flask setup is using sessions, but eventually I want to be able to
    store the JSON in a cookie that a user can return to in a new browser session.

--- test_flask.py
+++ test_flask.py
@@ -1,14 +1,25 @@
 import pythfinder as pf
 import json
-from flask import Flask, request, render_template
+from flask import Flask, session, request, render_template, make_response
 
 app = Flask(__name__)
+
+# Obviously not going to stay like this; this is for local dev only
+app.secret_key = "temporary_dev_secret_key"
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
         return render_template("form.html")
     else:
-        characterData = pf.readCharacterString(request.form["character"])
-        character = pf.getCharacterShort(characterData)
-        return render_template("index.html", character = character)
+        characterData = json.loads(request.form["character"])
+        character = pf.Character(data = characterData)
+        characterShort = character.getCharacterShort()
+        response = make_response(render_template("index.html", character = characterShort))
+        session["character"] = characterData
+
+        return response
+
+@app.route('/character/')
+def get_character():
+    return session["character"]
