90e6aa23e90d4dff5dfaf2adcaacc17107393560
Matt Hale <matthew.hale@protonmail.com>
2020-01-07 11:23:44 -0500

    Implement CharacterSkill class

--- pythfinder.py
+++ pythfinder.py
@@ -81,7 +81,11 @@ class Character:
             for item in data["savingThrows"].keys():
                 self.savingThrows[item] = CharacterSavingThrow(data = data["savingThrows"][item])
         
-        self.skills = data["skills"] if "skills" in keys else []
+        self.skills = []
+        if "skills" in keys:
+            for item in data["skills"]:
+                self.skills.append(CharacterSkill(data = item))
+
         self.attacks = data["attacks"] if "attacks" in keys else []
 
     # Get the modifier for a given ability
@@ -252,19 +256,80 @@ class CharacterSkill:
                  name = "",
                  rank = 0,
                  isClass = False,
-                 mod = "",
                  notes = "",
-                 useUntrained = True,
                  misc = 0,
                  data = {}):
         keys = data.keys()
+        allowedNames = (
+            "Acrobatics", "Appraise", "Bluff",
+            "Climb", "Craft", "Diplomacy",
+            "Disable Device", "Disguise", "Escape Artist",
+            "Fly", "Handle Animal", "Heal",
+            "Intimidate", "Knowledge (Arcana)", "Knowledge (Dungeoneering)",
+            "Knowledge (Engineering)", "Knowledge (Geography)", "Knowledge (History)",
+            "Knowledge (Local)", "Knowledge (Nature)", "Knowledge (Nobility)",
+            "Knowledge (Planes)", "Knowledge (Religion)", "Linguistics",
+            "Perception", "Perform", "Profession",
+            "Ride", "Sense Motive", "Sleight Of Hand",
+            "Spellcraft", "Stealth", "Survival",
+            "Swim", "Use Magic Device"
+        )
+        trainedOnly = (
+            "Disable Device", "Handle Animal", "Knowledge (Arcana)",
+            "Knowledge (Dungeoneering)", "Knowledge (Engineering)", "Knowledge (Geography)",
+            "Knowledge (History)", "Knowledge (Local)", "Knowledge (Nature)",
+            "Knowledge (Nobility)", "Knowledge (Planes)", "Knowledge (Religion)",
+            "Linguistics", "Perception", "Profession",
+            "Sleight Of Hand", "Spellcraft", "Use Magic Device"
+        )
+        mods = {
+            "Climb": "str",
+            "Swim": "str",
+            "Acrobatics": "dex",
+            "Disable Device": "dex",
+            "Escape Artist": "dex",
+            "Fly": "dex",
+            "Ride": "dex",
+            "Sleight Of Hand": "dex",
+            "Stealth": "dex",
+            "Appraise": "int",
+            "Craft": "int",
+            "Knowledge (Arcana)": "int",
+            "Knowledge (Dungeoneering)": "int",
+            "Knowledge (Engineering)": "int",
+            "Knowledge (Geography)": "int",
+            "Knowledge (History)": "int",
+            "Knowledge (Local)": "int",
+            "Knowledge (Nature)": "int",
+            "Knowledge (Nobility)": "int",
+            "Knowledge (Planes)": "int",
+            "Knowledge (Religion)": "int",
+            "Linguistics": "int",
+            "Spellcraft": "int",
+            "Heal": "wis",
+            "Perception": "wis",
+            "Profession": "wis",
+            "Sense Motive": "wis",
+            "Survival": "wis",
+            "Bluff": "cha",
+            "Diplomacy": "cha",
+            "Disguise": "cha",
+            "Handle Animal": "cha",
+            "Intimidate": "cha",
+            "Perform": "cha",
+            "Use Magic Device": "cha"
+        }
+
         self.name = data["name"] if "name" in keys else name
+        if not self.name in allowedNames:
+            raise ValueError("CharacterSkill: name must be one of " + allowedNames)
         self.rank = data["rank"] if "rank" in keys else rank
         self.isClass = data["isClass"] if "isClass" in keys else isClass
-        self.mod = data["mod"] if "mod" in keys else mod
         self.notes = data["notes"] if "notes" in keys else notes
-        self.useUntrained = data["useUntrained"] if "useUntrained" in keys else useUntrained
         self.misc = data["misc"] if "misc" in keys else misc
+
+        self.mod = mods[self.name]
+        self.useUntrained = False if self.name in trainedOnly else True
 
 class CharacterAttack:
     def __init__(self,
