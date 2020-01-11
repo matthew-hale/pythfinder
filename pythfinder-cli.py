8cf98ce5cfb98ed34485c265b7e663067dad62f2
Matt Hale <matthew.hale@protonmail.com>
2020-01-11 15:10:01 -0500

    Fix whitespace formatting for traits & feats in CLI

--- pythfinder-cli.py
+++ pythfinder-cli.py
@@ -27,14 +27,14 @@ def getEquipmentString(c):
 def getFeatString(c):
     outstring = "\n    Feats:\n\n    "
     for feat in c.feats:
-        outstring += "{}:\n        {}\n        {}\n    ".format(feat.name,feat.description,feat.notes)
+        outstring += "{}:\n        {}\n        {}\n\n    ".format(feat.name,feat.description,feat.notes)
     return outstring
 
 # Formatted string of traits
 def getTraitString(c):
     outstring = "\n    Traits:\n\n    "
     for trait in c.traits:
-        outstring +=  "{}:\n        {}\n        {}\n    ".format(trait.name,trait.description,trait.notes)
+        outstring +=  "{}:\n        {}\n        {}\n\n    ".format(trait.name,trait.description,trait.notes)
     return outstring
 
 # Formatted string of skills
