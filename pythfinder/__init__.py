c304452d9a6f677edb3686076c56fad18b7c6f80
Matt Hale <matthew.hale@protonmail.com>
2020-01-11 15:15:10 -0500

    Add CharacterArmor class

--- pythfinder/__init__.py
+++ pythfinder/__init__.py
@@ -95,6 +95,11 @@ class Character:
             for item in data["attacks"]:
                 self.attacks.append(CharacterAttack(data = item))
 
+        self.armor = []
+        if "armor" in keys:
+            for item in data["armor"]:
+                self.armor.append(CharacterArmor(data = item))
+
     # Get the modifier for a given ability
     def getAbilityMod(self, ability):
         abilityRange = ["str","dex","con","int","wis","cha"]
@@ -397,6 +402,23 @@ class CharacterAttack:
         self.critMulti = data["critMulti"] if "critMulti" in keys else critMulti
         self.range = data["range"] if "range" in keys else range_
         self.notes = data["notes"] if "notes" in keys else notes
+
+class CharacterArmor:
+    def __init__(self,
+                 name = "",
+                 acBonus = 0,
+                 acPenalty = 0,
+                 maxDexBonus = 0,
+                 arcaneFailureChance = 0,
+                 type_,
+                 data = {}):
+        keys = data.keys()
+        self.name = data["name"] if "name" in keys else name
+        self.acBonus = data["acBonus"] if "acBonus" in keys else acBonus
+        self.acPenalty = data["acPenalty"] if "acPenalty" in keys else acPenalty
+        self.maxDexBonus = data["maxDexBonus"] if "maxDexBonus" in keys else maxDexBonus
+        self.arcaneFailureChance = data["arcaneFailureChance"] if "arcaneFailureChance" in keys else arcaneFailureChance
+        self.type = data["type"] if "type" in keys else type_
 
 ### FUNCTIONS ###
 
