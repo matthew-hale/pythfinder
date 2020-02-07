6a08c764e4baf9f04da01dd602c478086c6b63d9
Matt Hale <matthew.hale@protonmail.com>
2020-02-07 16:17:55 -0500

    Add dest = "subparser_name" to subparsers
    I can now check which subcommand was called, as I intend to have a
    generic "target" positional parameter in most of the subcommands.

--- pythfinder-cli.py
+++ pythfinder-cli.py
@@ -164,7 +164,8 @@ dataChanged = False
 parser = argparse.ArgumentParser(description = "pathfinder 1E character sheet")
 
 # Subparsers for subcommands
-subparsers = parser.add_subparsers(help = "subcommand")
+subparsers = parser.add_subparsers(help = "subcommand",
+                                   dest = "subparser_name")
 parser_list = subparsers.add_parser("list",
                                     help = "list character details",
                                     aliases = ["ls"])
@@ -199,7 +200,6 @@ except FileNotFoundError:
 
 # Main execution
 arg = args.target
-print(args)
 if arg == "character":
     c = character.getCharacterShort()
     outstring = "\n    "
