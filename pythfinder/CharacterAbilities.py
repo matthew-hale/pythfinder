d1589704c2d474143d7e0be343903adf046e191f
Matt Hale <matthew.hale@protonmail.com>
2020-02-12 20:22:29 -0500

    Change structure of character abilities

--- pythfinder/CharacterAbilities.py
+++ pythfinder/CharacterAbilities.py
@@ -8,9 +8,9 @@ class CharacterAbilities:
                  cha = 10,
                  data = {}):
         keys = data.keys()
-        self.str = data["str"] if "str" in keys else str_
-        self.dex = data["dex"] if "dex" in keys else dex
-        self.con = data["con"] if "con" in keys else con
-        self.int = data["int"] if "int" in keys else int_
-        self.wis = data["wis"] if "wis" in keys else wis
-        self.cha = data["cha"] if "cha" in keys else cha
+        self.str = {"base": {data["str"], "mods": []} if "str" in keys else str_
+        self.dex = {"base": {data["dex"], "mods": []} if "dex" in keys else dex
+        self.con = {"base": {data["con"], "mods": []} if "con" in keys else con
+        self.int = {"base": {data["int"], "mods": []} if "int" in keys else int_
+        self.wis = {"base": {data["wis"], "mods": []} if "wis" in keys else wis
+        self.cha = {"base": {data["cha"], "mods": []} if "cha" in keys else cha_
