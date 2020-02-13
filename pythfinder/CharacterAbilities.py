43847dd2b2458e2f5c5ecf1dbb609b3626ccd3e7
Matt Hale <matthew.hale@protonmail.com>
2020-02-12 20:29:14 -0500

    Fix typo

--- pythfinder/CharacterAbilities.py
+++ pythfinder/CharacterAbilities.py
@@ -8,9 +8,9 @@ class CharacterAbilities:
                  cha = 10,
                  data = {}):
         keys = data.keys()
-        self.str = {"base": {data["str"], "mods": []} if "str" in keys else str_
-        self.dex = {"base": {data["dex"], "mods": []} if "dex" in keys else dex
-        self.con = {"base": {data["con"], "mods": []} if "con" in keys else con
-        self.int = {"base": {data["int"], "mods": []} if "int" in keys else int_
-        self.wis = {"base": {data["wis"], "mods": []} if "wis" in keys else wis
-        self.cha = {"base": {data["cha"], "mods": []} if "cha" in keys else cha_
+        self.str = {"base": data["str"], "mods": []} if "str" in keys else str_
+        self.dex = {"base": data["dex"], "mods": []} if "dex" in keys else dex
+        self.con = {"base": data["con"], "mods": []} if "con" in keys else con
+        self.int = {"base": data["int"], "mods": []} if "int" in keys else int_
+        self.wis = {"base": data["wis"], "mods": []} if "wis" in keys else wis
+        self.cha = {"base": data["cha"], "mods": []} if "cha" in keys else cha_
