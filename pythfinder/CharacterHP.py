469d43757f816bc6e19d8820ed4b7e0ab7065226
Matt Hale <matthew.hale@protonmail.com>
2020-02-14 20:44:10 -0500

    Add nonlethal damage to Character.hp
    Affects:
    
    * test.json
    * template.json
    * CharacterHP.py

--- pythfinder/CharacterHP.py
+++ pythfinder/CharacterHP.py
@@ -3,8 +3,10 @@ class CharacterHP:
                  max_ = 0,
                  current = 0,
                  temporary = 0,
+                 nonlethal = 0,
                  data = {}):
         keys = data.keys()
         self.max = data["max"] if "max" in keys else max_
         self.current = data["current"] if "current" in keys else current
         self.temporary = data["temporary"] if "temporary" in keys else temporary
+        self.nonlethal = data["nonlethal"] if "nonlethal" in keys else nonlethal
