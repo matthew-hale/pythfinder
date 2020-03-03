abb29f147819f4e2f5f42a21cb6bd29f332f8f75
Matt Hale <matthew.hale@protonmail.com>
2020-03-02 20:38:22 -0500

    Add filter_list function

--- pythfinder/__init__.py
+++ pythfinder/__init__.py
@@ -11,3 +11,18 @@ from pythfinder.Character import Character
 def writeCharacter(character, path):
     with open(path, "w", encoding="utf-8") as f:
         json.dump(character.getDict(), f, indent=4)
+
+# Filters the given list according to the provided two-tuples
+def filter_list(items, filters):
+    keys = items[0].keys()
+    # Prevent modification of "items" parameter
+    result = items.copy()
+    for tup in filters:
+        if not tup[0] in keys:
+            continue
+        # Prevent breakage of for loop
+        current_results = result.copy()
+        for item in current_results:
+            if item[tup[0]] != tup[1]:
+                result.remove(item)
+    return result
