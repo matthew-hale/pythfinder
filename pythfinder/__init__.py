e2e032ad9c318a399e6170ee60bde6e33f244158
Matt Hale <matthew.hale@protonmail.com>
2020-03-02 21:06:27 -0500

    Change filter_list to use dict for filters

--- pythfinder/__init__.py
+++ pythfinder/__init__.py
@@ -12,17 +12,17 @@ def writeCharacter(character, path):
     with open(path, "w", encoding="utf-8") as f:
         json.dump(character.getDict(), f, indent=4)
 
-# Filters the given list according to the provided two-tuples
+# Filters the given list according to the provided filter dictionary
 def filter_list(items, filters):
     keys = items[0].keys()
     # Prevent modification of "items" parameter
     result = items.copy()
-    for tup in filters:
-        if not tup[0] in keys:
+    for key in filters.keys():
+        if not key in keys:
             continue
         # Prevent breakage of for loop
         current_results = result.copy()
         for item in current_results:
-            if item[tup[0]] != tup[1]:
+            if item[key] != filters[key]:
                 result.remove(item)
     return result
