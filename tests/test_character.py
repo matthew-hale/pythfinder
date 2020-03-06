64296ce7ceb0f1f5cbfb90acdc80f9f62afb01f6
Matt Hale <matthew.hale@protonmail.com>
2020-03-06 12:11:53 -0500

    Convert tests to pytest

---
+++ tests/test_character.py
@@ -0,0 +1,72 @@
+#!/bin/python3
+#
+
+import pythfinder as pf
+
+# Initialize a character with some data for the basic parameters, and 
+# test that character's initialization, comparing its properties with 
+# those specified.
+character_data = {
+    "name": "test name",
+    "race": "human",
+    "deity": "god",
+    "homeland": "america",
+    "CMB": 5,
+    "CMD": 6,
+    "initiativeMods": [1, 2, 3],
+    "alignment": "CN",
+    "description": "test character description",
+    "height": "180cm",
+    "weight": "95kg",
+    "size": "M",
+    "age": 100,
+    "hair": "black",
+    "eyes": "purple",
+    "languages": ["common", "elven"],
+    "baseAttacBonus": [6, 1],
+    "gold": 10
+}
+
+character = pf.Character(data = character_data)
+
+# Are the keys of the character.getKeys() method the same as what 
+# they should be, as defined by should_dict?
+def test_character_keys():
+    should_dict = {
+        "name": "",
+        "race": "",
+        "deity": "",
+        "homeland": "",
+        "CMB": "",
+        "CMD": "",
+        "initiativeMods": "",
+        "alignment": "",
+        "description": "",
+        "height": "",
+        "weight": "",
+        "size": "",
+        "age": "",
+        "hair": "",
+        "eyes": "",
+        "languages": "",
+        "spellsPerDay": "",
+        "baseAttackBonus": "",
+        "gold": "",
+        "AC": "",
+        "speed": "",
+        "classes": "",
+        "abilities": "",
+        "hp": "",
+        "special": "",
+        "traits": "",
+        "feats": "",
+        "equipment": "",
+        "saving_throws": "",
+        "skills": "",
+        "spells": "",
+        "attacks": "",
+        "armor": ""
+    }
+    should = should_dict.keys()
+    actual = character.getDict().keys()
+    assert should == actual
