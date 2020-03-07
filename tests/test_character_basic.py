0be08d09e4a8a28ad39a4932e8ea914d498813a2
Matt Hale <matthew.hale@protonmail.com>
2020-03-07 15:04:33 -0500

    Add test to basic tests

--- tests/test_character_basic.py
+++ tests/test_character_basic.py
@@ -1,5 +1,6 @@
 #!/bin/python3
 
+from unittest.mock import Mock
 import pythfinder as pf
 
 # Generate test data
@@ -30,6 +31,28 @@ for i in range(5):
         "character": pf.Character(data = c_data),
         "data": c_data
     })
+
+# The add_* methods should not have been called, as our data didn't 
+# have any keys that would call them.
+def test_add_not_called():
+    # Mock add_* methods
+    pf.Character.add_class = Mock()
+    pf.Character.add_feat = Mock()
+    pf.Character.add_trait = Mock()
+    pf.Character.add_item = Mock()
+    pf.Character.add_special = Mock()
+    pf.Character.add_spell = Mock()
+    pf.Character.add_attack = Mock()
+    pf.Character.add_armor = Mock()
+
+    pf.Character.add_class.assert_not_called()
+    pf.Character.add_feat.assert_not_called()
+    pf.Character.add_trait.assert_not_called()
+    pf.Character.add_item.assert_not_called()
+    pf.Character.add_special.assert_not_called()
+    pf.Character.add_spell.assert_not_called()
+    pf.Character.add_attack.assert_not_called()
+    pf.Character.add_armor.assert_not_called()
 
 # Are the keys of the character.getKeys() method the same as what 
 # they should be, as defined by should_dict?
