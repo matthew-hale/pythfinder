7531fcbd5472de8268c7203a0519708bfa96d577
Matt Hale <matthew.hale@protonmail.com>
2020-03-07 15:48:41 -0500

    Add mocks to test_add_feat

--- tests/test_add_feat.py
+++ tests/test_add_feat.py
@@ -1,9 +1,19 @@
 #!/bin/python3
 
+from unittest.mock import Mock
 import pythfinder as pf
+import pytest
+
+# Define mocks in setup function
+@pytest.fixture()
+def mock_setup():
+    pf.Character.__init__ = Mock()
+    pf.Character.__init__.return_value = None
+    pf.Character.is_unique_name = Mock()
+    pf.Character.is_unique_name.return_value = True
 
 # Does add_feat return what we expect?
-def test_add_feat():
+def test_add_feat(mock_setup):
     # Add a feat to a character
     feat_data = {
         "name": "feat name",
@@ -12,6 +22,7 @@ def test_add_feat():
     }
     
     c = pf.Character()
+    c.feats = []
     feat = c.add_feat(data = feat_data)
 
     for key in feat_data.keys():
