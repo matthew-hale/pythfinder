b30a1522b480ca3e6a7affa53bb79972022f7bd2
Matt Hale <matthew.hale@protonmail.com>
2020-03-08 13:24:52 -0400

    Add tests for remaining add_* methods

---
+++ tests/test_add_attack.py
@@ -0,0 +1,52 @@
+#!/bin/python3
+
+from unittest.mock import Mock
+import pythfinder as pf
+import pytest
+
+# Define mocks in setup function
+@pytest.fixture()
+def setup_char():
+    pf.Character.__init__ = Mock()
+    pf.Character.__init__.return_value = None
+    pf.Character.is_unique_name = Mock()
+    pf.Character.is_unique_name.return_value = True
+    # Add an attack to a character
+    attack_data = {
+        "name": "attack name",
+        "attackType": "attack type",
+        "damageType": [],
+        "damage": "attack damage",
+        "critRoll": 18,
+        "critMulti": 4,
+        "range": 55,
+        "notes": "attack notes"
+    }
+    
+    c = pf.Character()
+    c.attacks = []
+    attack = c.add_attack(data = attack_data)
+
+    return (attack_data, attack, c)
+
+# Is the attack that the method returns the same as what we put in?
+def test_add_attack_return(setup_char):
+    attack_data = setup_char[0]
+    attack = setup_char[1]
+    for key in attack_data.keys():
+        assert attack[key] == attack_data[key]
+
+# Is the actual attack in the character data the same as what we put in?
+def test_add_attack_actual(setup_char):
+    attack_data = setup_char[0]
+    c = setup_char[2]
+    for key in attack_data.keys():
+        assert c.attacks[0][key] == attack_data[key]
+
+# Is the actual attack in the character data the same as the one that 
+# was returned?
+def test_add_attack_method(setup_char):
+    attack = setup_char[1]
+    c = setup_char[2]
+    for key in attack.keys():
+        assert c.attacks[0][key] == attack[key]
---
+++ tests/test_add_item.py
@@ -0,0 +1,49 @@
+#!/bin/python3
+
+from unittest.mock import Mock
+import pythfinder as pf
+import pytest
+
+# Define mocks in setup function
+@pytest.fixture()
+def setup_char():
+    pf.Character.__init__ = Mock()
+    pf.Character.__init__.return_value = None
+    pf.Character.is_unique_name = Mock()
+    pf.Character.is_unique_name.return_value = True
+    # Add an item to a character
+    item_data = {
+        "name": "item name",
+        "weight": 5.25,
+        "count": 2,
+        "pack": True,
+        "notes": "item notes"
+    }
+    
+    c = pf.Character()
+    c.equipment = []
+    item = c.add_item(data = item_data)
+
+    return (item_data, item, c)
+
+# Is the item that the method returns the same as what we put in?
+def test_add_item_return(setup_char):
+    item_data = setup_char[0]
+    item = setup_char[1]
+    for key in item_data.keys():
+        assert item[key] == item_data[key]
+
+# Is the actual item in the character data the same as what we put in?
+def test_add_item_actual(setup_char):
+    item_data = setup_char[0]
+    c = setup_char[2]
+    for key in item_data.keys():
+        assert c.equipment[0][key] == item_data[key]
+
+# Is the actual item in the character data the same as the one that was 
+# returned?
+def test_add_item_method(setup_char):
+    item = setup_char[1]
+    c = setup_char[2]
+    for key in item.keys():
+        assert c.equipment[0][key] == item[key]
