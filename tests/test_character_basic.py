bc6c2a6b7e99f086e17b46e257865c7f02a12c52
Matt Hale <matthew.hale@protonmail.com>
2020-11-25 18:54:53 -0500

    Fix test for new character property 'bonuses'

--- tests/test_character_basic.py
+++ tests/test_character_basic.py
@@ -78,6 +78,7 @@ def test_character_keys():
         "baseAttackBonus": "",
         "gold": "",
         "AC": "",
+        "bonuses": "",
         "speed": "",
         "classes": "",
         "abilities": "",
