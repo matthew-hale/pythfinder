075905b90d04d89d2408c3c63f935b186d1d4b45
Matt Hale <matthew.hale@protonmail.com>
2020-11-27 17:02:21 -0500

    Rework 'roll' function to allow for arbitrary number of dice and modifiers

--- pythfinder/roll.py
+++ pythfinder/roll.py
@@ -7,37 +7,32 @@ import json
 #     pythfinder.roll("2d4")
 #     pythfinder.roll("1d20+5")
 #     pythfinder.roll("3d8+5+2")
+#     pythfinder.roll("2d6+4-7+12-9")
+#
+# Returns integer value of final result
 def roll(roll_string = ""):
-    # Parse roll string
-    match = re.search("^[0-9]+d[0-9]+\+?[0-9]+$", roll_string)
-    if match:
-        roll = match.group(0)
-    else:
-        return None
+    # regex strings
+    dice_regex = "[0-9]+d[0-9]+"
+    modifier_regex = "\+[0-9]+|\-[0-9]+"
 
-    bonus = re.search("\+", roll)
+    # Parse roll string, getting dice and all modifiers
+    dice = re.findall(dice_regex, roll_string)
+    modifiers = re.findall(modifier_regex, roll_string)
 
-    # Parse the string, including optional bonus
-    roll_bonus = 0
-    if bonus:
-        # the number of dice comes before the "d", and the size of the 
-        # dice comes right after, but before any plus signs
-        roll_count = int(roll.split("d")[0])
-        roll_size = int(roll.split("d")[1].split("+")[0])
-        roll_bonus = int(roll.split("d")[1].split("+")[1])
-    else:
-        roll_split = roll.split("d")
-        roll_count = int(roll_split[0])
-        roll_size = int(roll_split[1])
+    if not dice:
+        raise ValueError("roll: improper dice format")
 
-    total = 0
-    for _ in range(roll_count):
-        if roll_size == 1:
-            result = 1
-        else:
-            result = random.randrange(1,roll_size)
-        total += result
+    subtotal = 0
+    for die in dice:
+        dice_count = int(re.search("^[0-9]*", die).group(0))
+        dice_size = int(re.search("[0-9]*$", die).group(0))
+    
+        # Sum each dice roll
+        for d in range(dice_count):
+            subtotal += random.randrange(1,dice_size)
+    # Sum each modifier
+    if modifiers:
+        for m in modifiers:
+            subtotal += int(m)
 
-    total += roll_bonus 
-
-    return total
+    return subtotal
