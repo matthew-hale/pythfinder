f5b34f6895f809e60376d25f15304b07d800cc81
Matt Hale <matthew.hale@protonmail.com>
2020-12-20 14:41:44 -0500

    Break out helper functions and vars into separate files

--- pythfinder/__init__.py
+++ pythfinder/__init__.py
@@ -1,14 +1,2 @@
-#!/bin/python3
-#
-# pythfinder.py
-
-import json
-from pythfinder.Character import Character
-
-### FUNCTIONS ###
-
-# Write the given character data to the file in path
-def writeCharacter(character, path):
-    with open(path, "w", encoding="utf-8") as f:
-        json.dump(character.getDict(), f, indent=4)
-
+from .character import Character
+from .helpers import writeCharacter
