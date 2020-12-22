0d0be4ce00f5e5022bab4fec28ed639282575f8f
Matt Hale <matthew.hale@protonmail.com>
2020-12-22 17:55:55 -0500

    Add Equipment class

--- pythfinder/collections.py
+++ pythfinder/collections.py
@@ -149,3 +149,76 @@ class CharacterClass:
         if archetypes is not None:
             self.archetypes = archetypes
         return self
+
+"""
+An object representing a piece of equipment owned by the character. 
+Equipment has a per-unit weight, and a count; a location value; an 
+'on_person' flag; and a flag to determine whether or not to leave it 
+behind when the player makes camp.
+"""
+class Equipment:
+    def __init__(self,
+                 name = "",
+                 uuid = "",
+                 weight = 0.0,
+                 count = 0,
+                 camp = False,
+                 on_person = False,
+                 location = "",
+                 notes = "",
+                 data = {}):
+        keys = data.keys()
+        self.name = data["name"] if "name" in keys else name
+        self.weight = data["weight"] if "weight" in keys else weight
+        self.count = data["count"] if "count" in keys else count
+        self.camp = data["camp"] if "camp" in keys else camp
+        self.on_person = data["on_person"] if "on_person" in keys else on_person
+        self.location = data["location"] if "location" in keys else location
+        self.notes = data["notes"] if "notes" in keys else notes
+        self.uuid = data["uuid"] if "uuid" in keys else uuid
+
+        if not self.uuid:
+            self.uuid = str(uuid4())
+
+    """
+    Compare attributes excluding uuid
+    """
+    def __eq__(self, other):
+        if not isinstance(other, Equipment):
+            return NotImplemented
+        keys = ["name", "weight", "count", "camp", "on_person", "location", "notes"]
+        return all([True for key in keys if getattr(self, key) == getattr(other, key)])
+
+    def update(self,
+               name = None,
+               weight = None,
+               count = None,
+               camp = None,
+               on_person = None,
+               location = None,
+               notes = None,
+               data = {}):
+        keys = data.keys()
+        name = data["name"] if "name" in keys else name
+        weight = data["weight"] if "weight" in keys else weight
+        count = data["count"] if "count" in keys else count
+        camp = data["camp"] if "camp" in keys else camp
+        on_person = data["on_person"] if "on_person" in keys else on_person
+        location = data["location"] if "location" in keys else location
+        notes = data["notes"] if "notes" in keys else notes
+        # Ignore parameters not provided, allowing for "falsey" values
+        if name is not None:
+            self.name = name
+        if weight is not None:
+            self.weight = weight
+        if count is not None:
+            self.count = count
+        if camp is not None:
+            self.camp = camp
+        if on_person is not None:
+            self.on_person = on_person
+        if location is not None:
+            self.location = location
+        if notes is not None:
+            self.notes = notes
+        return self
