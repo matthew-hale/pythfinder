c079587345daa318819dfb0536e05381dfc43cc2
Matt Hale <matthew.hale@protonmail.com>
2020-12-23 22:18:50 -0500

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as fh:
 
 setuptools.setup(
     name="pythfinder",
-    version="0.15.0",
+    version="0.15.1",
     author="Matthew Hale",
     author_email="matthew.hale@protonmail.com",
     description="A pathfinder character sheet python package",
